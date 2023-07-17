077758edac4a193e0774c07e65a03941222d52cf
Yasengo <yonghangharka9@gmail.com>
2023-07-17 20:18:39 +0545

    mass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Pizza />
+    </div>
+  );
+}
+//component
+function Pizza() {
+  return <h1>this is spizza</h1>;
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
