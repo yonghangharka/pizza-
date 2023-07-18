e4d4563815d8ebbaf11abfb6c52a6e4c6b30420b
Yasengo <yonghangharka9@gmail.com>
2023-07-18 12:13:41 +0545

    react app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
-//component
-function Pizza() {
-  return <h1>this is spizza</h1>;
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>fast react Pizza co.</h1>
+    </header>
+  );
 }
+function Menu() {
+  return (
+    <main className="menu">
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, ham, aragula,and burrata cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Finghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={20}
+      />
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>{new Date().toLocaleTimeString()}. We are open.</p>
+    </footer>
+  );
+}
+//components
+function Pizza(props) {
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h2>{props.name}</h2>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
